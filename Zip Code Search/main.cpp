1db06e48f9c671c85f6fcf5659cda4f04bf3ef44
Stephen Moon <student@smoon2016.hotchkiss.org>
2012-09-24 22:21:25 -0400

    Initial Commit

---
+++ Zip Code Search/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Zip Code Search
+//
+//  Created by Stephen Moon on 9/24/12.
+//  Copyright (c) 2012 Stephen Moon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
